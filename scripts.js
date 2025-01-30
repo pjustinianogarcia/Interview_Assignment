8f35becd6c6a7597cd9149a200c6a109293e585d
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2025-01-29 20:20:49 -0400

    Created function to fetch images

---
+++ scripts.js
@@ -0,0 +1,17 @@
+fetch('https://fakestoreapi.com/products?limit=9')
+            .then(response=>response.json())
+            .then(json=> { 
+
+const imageContainer = document.getElementById("imageContainer");
+const imageGrid = document.getElementById("imageGrid");
+
+json.forEach(product => {
+    const image = document.createElement("img")
+    image.src = product.image;
+    image.alt = product.tittle
+
+    imageGrid.appendChild(image)
+
+
+})
+})
