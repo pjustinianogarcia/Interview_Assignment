4c98d581e4af0707fe39f80232489f022ebae880
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2025-01-29 21:39:12 -0400

    Added styles

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ fetch('https://fakestoreapi.com/products?limit=9')
             .then(response=>response.json())
             .then(json=> { 
 
-const imageContainer = document.getElementById("imageContainer");
+
 const imageGrid = document.getElementById("imageGrid");
 
 json.forEach(product => {
    const productCard = document.createElement("div");
-   
+   productCard.classList.add("productCard")
 
     const image = document.createElement("img");
     image.src = product.image;
@@ -16,9 +16,11 @@ json.forEach(product => {
     
 
     const title = document.createElement("div");
+    title.classList.add("title")
     title.textContent = product.title
 
     const price = document.createElement("div")
+    price.classList.add("price")
     price.textContent = `$${product.price.toFixed(2)}`
 
     productCard.appendChild(image);
